aa19c5f7cd24f05b26fd07d26f821ab54e951984
monojmkd <monoj.mkd@hotmail.com>
2025-03-26 23:39:27 +0530

    fresh commit

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('static'));
 
+// GET all employees
 app.get('/employees', async (req, res) => {
   const employees = getAllEmployees();
   res.json({ employees });
